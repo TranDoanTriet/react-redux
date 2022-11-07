6b15d2201933e4f1bd7c1ec15032e7f6664b43ce
TranDoanTriet <trandoantriet@gmail.com>
2022-11-07 15:45:12 +0700

    videoo 16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+// class component
+// function component
+import React from "react";
+class MyComponent extends React.Component{
+    //jsx
+    render(){
+        return (
+            <div>my first component</div>
+        )
+    }
+}
+
+export default MyComponent;
